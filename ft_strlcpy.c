85aa29bd22b0b925526dde6c1582d158e8df38e6
Utharushan <Utharushan@gmail.com>
2024-08-13 16:23:42 +0200

    ft_strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -11,3 +11,22 @@
 /* ************************************************************************** */
 #include "libft.h"
 
+int	ft_strlcpy(char *dest, char *src, int size)
+{
+	int	i;
+	int	len_src;
+
+	i = 0;
+	len_src = 0;
+	while (src[len_src])
+		len_src++;
+	if (!size)
+		return (len_src);
+	while (src[i] && (i < size - 1))
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (len_src);
+}
