3877a2e9d9a3287e887ec53c9721b77070f8b3b1
Utharushan <Utharushan@gmail.com>
2024-11-13 23:41:33 +0100

    libft

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -10,6 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
+#include <stddef.h>
 
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -10,6 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
+#include <stddef.h>
 
 size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 {
