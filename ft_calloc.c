5bc3f9e1e31becde9f60b3ce789f90d610c1a50c
Utharushan <Utharushan@gmail.com>
2024-11-17 12:12:04 +0100

    libft

--- ft_calloc.c
+++ ft_calloc.c
@@ -14,18 +14,14 @@
 void	*ft_calloc(size_t count, size_t size)
 {
 	void	*ptr;
-	size_t	i;
-	char	*temp;
+	size_t	total_size;
 
-	ptr = malloc(count * size);
+	if (count && SIZE_MAX / count < size)
+		return (NULL);
+	total_size = count * size;
+	ptr = malloc(total_size);
 	if (!ptr)
 		return (NULL);
-	temp = (char *)ptr;
-	i = 0;
-	while (i < count * size)
-	{
-		temp[i] = 0;
-		i++;
-	}
+	ft_bzero(ptr, total_size);
 	return (ptr);
 }
--- libft.h
+++ libft.h
@@ -18,6 +18,7 @@
 # include <unistd.h>
 # include <stdio.h>
 # include <stddef.h>
+# include <stdint.h>
 
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
